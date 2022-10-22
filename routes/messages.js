520ce5de84dd5fc842c00a14bebd41ef4121222d
grietbeyens <beyensgriet@gmail.com>
2022-10-22 17:21:58 +0200

    feat: get message by id

--- routes/messages.js
+++ routes/messages.js
@@ -2,17 +2,17 @@ var express = require("express");
 var router = express.Router();
 
 const messages = [
-  "Hello, world!",
-  "I am a message!",
-  "I am a message too!",
-  "I am a message three!",
-  "I am a message four!",
-  "I am a message five!",
-  "I am a message six!",
-  "I am a message seven!",
-  "I am a message eight!",
-  "I am a message nine!",
-  "I am a message ten!",
+  { message: "Hello, world!", user: "Bob" },
+  { message: "I am a message!", user: "Alice" },
+  { message: "I am a message too!", user: "Fred" },
+  { message: "I am a message three!", user: "Bob" },
+  { message: "I am a message four!", user: "Alice" },
+  { message: "I am a message five!", user: "Fred" },
+  { message: "I am a message six!", user: "Layla" },
+  { message: "I am a message seven!", user: "Alice" },
+  { message: "I am a message eight!", user: "Layla" },
+  { message: "I am a message nine!", user: "Bert" },
+  { message: "I am a message ten!", user: "Charles" },
 ];
 
 const getResponse = {
@@ -23,6 +23,18 @@ const getResponse = {
 
 /* GET api/v1/messages */
 router.get("/", function (req, res, next) {
+  res.json(getResponse);
+});
+
+router.get("/:id", function (req, res, next) {
+  const { id } = req.params; // const id = req.params.id;
+  console.log("id", id);
+  const message = messages[id];
+  const getResponse = {
+    status: "success",
+    message: `getting message with id ${id}`,
+    data: { message },
+  };
   res.json(getResponse);
 });
 
